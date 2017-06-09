b4f079a68d7c463b9a6a5de53122a33ae9442e89
root <root@arbildolinux.com>
2017-06-08 22:41:43 -0500

    Primera Presentación

---
+++ app/src/main/java/com/example/arbildo/proyecto/Navegacion.java
@@ -0,0 +1,120 @@
+package com.example.arbildo.proyecto;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.login.widget.ProfilePictureView;
+
+public class Navegacion extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    String id, jnombre, jcorreo;
+    ProfilePictureView profilePictureView;
+    TextView cajanombre, cajacorreo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navegacion);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        id = getIntent().getStringExtra("IdUsuario");
+        jnombre= getIntent().getStringExtra("NombreUsuario");
+        jcorreo= getIntent().getStringExtra("EmailUsuario");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView= navigationView.getHeaderView(0);
+        navigationView.setNavigationItemSelectedListener(this);
+        cajanombre=(TextView) findViewById(R.id.tvnombre);
+        cajacorreo=(TextView) findViewById(R.id.tvcorreo);
+        profilePictureView.setProfileId(id);
+        cajanombre.setText(jnombre);
+        cajacorreo.setText(jcorreo);
+
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navegacion, menu);
+
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_menu) {
+            // Handle the camera action
+        } else if (id == R.id.nav_pedidos) {
+
+        } else if (id == R.id.nav_micuenta) {
+
+        } else if (id == R.id.nav_salir) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
--- app/src/main/java/com/example/arbildo/proyecto/SegundoActivity.java
+++ app/src/main/java/com/example/arbildo/proyecto/SegundoActivity.java
@@ -1,14 +1,19 @@
 package com.example.arbildo.proyecto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.login.widget.ProfilePictureView;
 
-public class SegundoActivity extends AppCompatActivity {
+public class SegundoActivity extends AppCompatActivity{
     ProfilePictureView profilePictureView;
     TextView nombre, correo;
+    Button botoncontinuar;
+    String id, jnombre, jcorreo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,17 +22,24 @@ public class SegundoActivity extends AppCompatActivity {
         profilePictureView = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
         nombre=(TextView) findViewById(R.id.tvnombre);
         correo=(TextView) findViewById(R.id.tvcorreo);
-
-        String id = getIntent().getStringExtra("IdUsuario");
-        String jnombre= getIntent().getStringExtra("NombreUsuario");
-        String jcorreo= getIntent().getStringExtra("EmailUsuario");
-
+        id = getIntent().getStringExtra("IdUsuario");
+        jnombre= getIntent().getStringExtra("NombreUsuario");
+        jcorreo= getIntent().getStringExtra("EmailUsuario");
+        botoncontinuar=(Button) findViewById(R.id.btnContinuar);
+        botoncontinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inav = new Intent(getApplicationContext(), Navegacion.class);
+                inav.putExtra("IdUsuario", id);
+                inav.putExtra("NombreUsuario",jcorreo);
+                inav.putExtra("EmailUsuarion",jnombre);
+                startActivity(inav);
+            }
+        });
 
         profilePictureView.setProfileId(id);
         nombre.setText(jnombre);
         correo.setText(jcorreo);
     }
-
-
 
 }
